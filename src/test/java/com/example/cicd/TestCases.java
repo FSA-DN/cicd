776e9b8666d0da694cc08d97ed15a6fe5a55628c
Tariq <logn.academy@outlook.com>
2024-10-14 12:17:12 +0700

    Test cases added

---
+++ src/test/java/com/example/cicd/TestCases.java
@@ -0,0 +1,26 @@
+package com.example.cicd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCases {
+
+    @Test
+    public void testAlwaysPass1() {
+        // This test will always pass
+        assertTrue(true, "This test always passes.");
+    }
+
+    @Test
+    public void testAlwaysPass2() {
+        // This test will always pass
+        assertTrue(true, "This test always passes.");
+    }
+
+    @Test
+    public void testAlwaysPass3() {
+        // This test will always pass
+        assertTrue(true, "This test always passes.");
+    }
+}
