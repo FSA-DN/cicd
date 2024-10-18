03106a1333b7f9dcd46d432494ee159dfb5fe6ac
Tariq <logn.academy@outlook.com>
2024-10-18 09:45:50 +0700

    Fail test

--- src/test/java/com/example/cicd/TestCases.java
+++ src/test/java/com/example/cicd/TestCases.java
@@ -21,6 +21,6 @@ public class TestCases {
     @Test
     public void test3() {
         // This test will always pass
-        assertTrue(true, "This test always passes.");
+        assertTrue(false, "This test always passes.");
     }
 }
